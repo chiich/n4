b3e99510d3ec3a3b34db530f1ce78d59b70c6714
Chie Ichongiri <Chiemezie.Ichongiri@wlt.com>
2020-08-15 18:47:20 +0100

    Introducing HTTP2 server

--- app.js
+++ app.js
@@ -3,9 +3,15 @@ const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
+const http2 = require('http2');
+const fs = require('fs');
 
 const app = express();
 const port = process.env.PORT || 3000;
+const options = {
+  key: fs.readFileSync(path.join(__dirname, '/localhost-cert.pem')),
+  cert: fs.readFileSync(path.join(__dirname, '/localhost-privkey.pem')),
+};
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public/')));
@@ -44,6 +50,13 @@ app.post('/', (req, res) => {
   res.status(201).end();
 });
 
-app.listen(port, () => {
-  debug(`Listening on port ${chalk.green(port)}`);
-});
+http2
+  .createSecureServer(options, app)
+  .listen(port, (error) => {
+    if (error) {
+      debug(chalk.red(error));
+      process.exit(1);
+    } else {
+      debug(`Listening on port ${chalk.green(port)}`);
+    }
+  });
