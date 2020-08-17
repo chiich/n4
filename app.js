028988d17c855b0538a8c23925a6a51d8ca5fef7
Chie <ichongiribc@gmail.com>
2020-08-17 19:02:17 +0100

    Replaced http2 with spdy; added helmet; and included intermediate page, '/processing'

--- app.js
+++ app.js
@@ -2,17 +2,23 @@ const express = require('express');
 const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
+const helmet = require('helmet');
 const path = require('path');
-const http2 = require('http2');
+const spdy = require('spdy');
 const fs = require('fs');
 
 const app = express();
 const port = process.env.PORT || 3000;
-const options = {
+const http2Options = {
   cert: fs.readFileSync(path.join(__dirname, '/localhost-cert.pem')),
   key: fs.readFileSync(path.join(__dirname, '/localhost-privkey.pem')),
 };
-
+const staticOptions = {
+  setHeaders: function(res, path, stat) {
+    res.set('x-timestamp', Date.now());
+  }
+}
+app.use(helmet());
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public/')));
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css/')));
@@ -33,10 +39,21 @@ const nav = [{
 }];
 const bookRouter = require('./src/routes/books.route')(nav);
 
+app.use(function(req, res, next) {
+  next();
+});
 app.use('/books', bookRouter);
+app.get('/processing', (req, res) => {
+  res.set({
+    'Content-Type': 'application/html',
+    Location: '/books',
+    'Content-length': 0,
+    'Expires': 0,
+  });
+  res.status(302).end();
+});
 app.get('/', (req, res) => {
-  // res.send('Hello from the express server!');
-  // res.sendFile(path.join(__dirname, 'views/index.html'));
+  req.headers['x-timestamp'] = Date.now();
   res.render('index', {
     title: 'Cheese App',
     nav,
@@ -44,14 +61,16 @@ app.get('/', (req, res) => {
 });
 app.post('/', (req, res) => {
   res.set({
-    Location: '/books',
-    Refresh: '0; url=/books',
+    Location: '/processing',
+    Refresh: '0; url=/processing',
+    'Content-length': 0,
+    'last-modified': Date.now(),
   });
   res.status(201).end();
 });
 
-http2
-  .createSecureServer(options, app)
+spdy
+  .createServer(http2Options, app)
   .listen(port, (error) => {
     if (error) {
       debug(chalk.red(error));
