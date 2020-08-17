69c4219e980a6695313b3782dc287b97e7c49946
Chie Ichongiri <Chiemezie.Ichongiri@wlt.com>
2020-08-17 19:49:57 +0100

    Adding line to disable 'x-powered-by' using express instance, despite helmet already capable of doing so. Reason is it's good practice, and decouples that requirement from a module which could be taken out at any time

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist
 app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist/')));
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
+app.disable('x-powered-by');
 
 const nav = [{
   title: 'Books',
